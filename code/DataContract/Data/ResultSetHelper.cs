a5afada82a1f9f2c4465edfddf61d8891e426dcd
vplauzon <vincent_lauzon@yahoo.com.au>
2025-04-03 01:49:49 -0400

    .

---
+++ code/DataContract/Data/ResultSetHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContract.Data
+{
+    internal static class ResultSetHelper
+    {
+        public static async Task<DataSet> ToDataSetAsync(this ResultSet resultSet)
+        {
+            var dataSet = new DataSet();
+
+            await foreach (var tableSet in resultSet.GetTableSetsAsync())
+            {
+                var table = new DataTable(tableSet.Name);
+
+                //table.Columns.AddRange();
+
+                dataSet.Tables.Add(table);
+            }
+
+            return dataSet;
+        }
+
+        public static async Task<DataTable> ToDataTableAsync(this ResultSet resultSet)
+        {
+            var dataSet = await resultSet.ToDataSetAsync();
+
+            if(dataSet.Tables.Count == 0)
+            {
+                throw new InvalidDataException("DataSet has no table");
+            }
+
+            return dataSet.Tables[0];
+        }
+    }
+}
