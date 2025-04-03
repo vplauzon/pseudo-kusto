e5b8df21fc456b03ee1f829f3f81eb1ae27615a5
vplauzon <vincent_lauzon@yahoo.com.au>
2025-04-03 01:37:30 -0400

    .

--- Code/DataContract/Data/ResultSet.cs
+++ Code/DataContract/Data/ResultSet.cs
@@ -2,6 +2,25 @@
 {
     public class ResultSet
     {
-        IAsyncEnumerable<ITableSet> GetTableSetsAsync();
+        private readonly IAsyncEnumerable<ITableSet> _tableSets;
+        private volatile int _getCount = 0;
+
+        public ResultSet(IAsyncEnumerable<ITableSet> tableSets)
+        {
+            _tableSets = tableSets;
+        }
+
+        public IAsyncEnumerable<ITableSet> GetTableSetsAsync()
+        {
+            if (Interlocked.CompareExchange(ref _getCount, 1, 0) == 0)
+            {
+                return _tableSets;
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    "Table set has already been accessed and can only be accessed once");
+            }
+        }
     }
 }
