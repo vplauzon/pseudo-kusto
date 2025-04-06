0dc7d8d6db2af0fc4870732146ceefbb41d60330
vplauzon <vincent_lauzon@yahoo.com.au>
2025-04-05 21:46:14 -0400

    .

---
+++ code/QueryPlan/Parsing/Script.cs
@@ -0,0 +1,6 @@
+﻿namespace QueryPlan.Parsing
+{
+    public class Script
+    {
+    }
+}
---
+++ code/QueryPlan/Parsing/ScriptParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryPlan.Parsing
+{
+    public static class ScriptParser
+    {
+        public static Script ParseScript(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
